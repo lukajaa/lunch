64d1c2586bb0bec2c88485772a3e5550cb87fec7
lukajaa <lucaskchang@gmail.com>
2023-03-28 20:39:25 -0700

    CNAME

--- app.vue
+++ app.vue
@@ -5,37 +5,35 @@
       <div class="flex flex-row mt-10">
         <div class="flex flex-col w-6/12">
           <img v-if="image == 'menu'" src="~/assets/images/menu.jpg" class="rounded-xl rotate-[-12deg] transition ease-in-out hover:scale-110 hover:shadow-xl mt-10" >
-          <img v-else-if="image == 'dog'" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftownsquare.media%2Fsite%2F40%2Ffiles%2F2017%2F03%2FDog-.jpg%3Fw%3D1200%26h%3D0%26zc%3D1%26s%3D0%26a%3Dt%26q%3D89&f=1&nofb=1&ipt=0bed59dc95a604b179d40f50fbdce71bd2e998da605eac60f1f0bdb831e9b532&ipo=images" class="rounded-xl rotate-[-12deg] transition ease-in-out hover:scale-110 hover:shadow-xl mt-10" >
+          <img v-else-if="image == 'dog'" src="https://i.insider.com/553030256bb3f7d747e8a4d5" class="rounded-xl rotate-[-12deg] transition ease-in-out hover:scale-110 hover:shadow-xl mt-10" >
         </div>
         <div class="flex flex-col w-6/12">
-          <p>
-            Most Common Words: <br />
-            1. Seasonal: 123 <br />
-            2. Vegetables: 108 <br />
-            3. Salad: 93 <br />
-            4. Rice: 76 <br />
-            5. Chicken: 69 <br />
-            6. Organic: 48 <br />
-            7. Cheese: 45 <br />
-            8. Veggie: 42 <br />
-            9. Tofu: 32 <br />
-            10. Beef: 29 <br />
-            11. Housemade: 29 <br />
-            12. Roasted: 27 <br />
-            13. Soup: 26 <br />
-            14. Pasta: 25 <br />
-            15. Niman: 23 <br />
-            16. Ranch: 23 <br />
-            17. Marinara: 20 <br />
-            <a @click="image = 'dog'">18. Dogs: 20 </a><br />
-            19. Pizza: 20 <br />
-            20. Sauce: 19 <br />
-            21. Kale: 18 <br />
-            22. Vegetable: 18 <br />
-            23. Quesadillas: 17 <br />
-            24. Bread: 17 <br />
-            25. Bean: 16 <br />
-          </p>
+          Most Common Words: <br />
+          1. Seasonal: 123 <br />
+          2. Vegetables: 108 <br />
+          3. Salad: 93 <br />
+          4. Rice: 76 <br />
+          5. Chicken: 69 <br />
+          6. Organic: 48 <br />
+          7. Cheese: 45 <br />
+          8. Veggie: 42 <br />
+          9. Tofu: 32 <br />
+          10. Beef: 29 <br />
+          11. Housemade: 29 <br />
+          12. Roasted: 27 <br />
+          13. Soup: 26 <br />
+          14. Pasta: 25 <br />
+          15. Niman: 23 <br />
+          16. Ranch: 23 <br />
+          17. Marinara: 20 <br />
+          <a @click="image = 'dog'">18. Dogs: 20 </a>
+          19. Pizza: 20 <br />
+          20. Sauce: 19 <br />
+          21. Kale: 18 <br />
+          22. Vegetable: 18 <br />
+          23. Quesadillas: 17 <br />
+          24. Bread: 17 <br />
+          25. Bean: 16 <br />
         </div>
       </div>
       <div class="flex flex-row mt-10">
@@ -91,7 +89,7 @@
         </div>
       </div>
       <div class="pt-10">
-        Created by <a href="https://www.lucaskchang.com" target="_blank" class="text-blue-500 hover:text-blue-700">Lucas Chang</a>. Made with Nuxt.js and Tailwind CSS.
+        Created by <a href="https://www.lucaskchang.com" target="_blank" class="text-blue-500 hover:text-blue-700">Lucas Chang</a>. Made with <a href="https://github.com/lukajaa/lunch" target="_blank">Nuxt.js and Tailwind CSS</a>.
       </div>
     </div>
   </div>
